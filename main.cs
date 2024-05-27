c19be468ed3200dc153959f766ffbbbba4b138e7
Cannonbarrel <Firenoob1001@gmail.com>
2024-05-27 00:29:05 +0000

    My program for Exercise 1.1

--- main.cs
+++ main.cs
@@ -1,7 +1,17 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World Zachary is here!");
-  }
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = "Lucinda Potter";
+        string birthdate = "6/24/1992";
+        string workPhone = "000-000-0101";
+        string cellPhone = "000-000-0189";
+
+        Console.WriteLine(name);
+        Console.WriteLine(birthdate);
+        Console.WriteLine("work " + workPhone);
+        Console.WriteLine("cell " + cellPhone);
+    }
 }
